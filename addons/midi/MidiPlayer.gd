3b0a0956405d7612be73b1b0f90cccdbf04c1426
あるる / きのもと 結衣 <arleyuti@gmail.com>
2019-12-18 11:23:34 +0900

    portamentoの綴りミス修正

--- addons/midi/MidiPlayer.gd
+++ addons/midi/MidiPlayer.gd
@@ -699,7 +699,7 @@ func _process_track_event_control_change( channel:GodotMIDIPlayerChannelStatus, 
 			channel.hold = 64 <= value
 			self._apply_channel_hold( channel )
 		SMF.control_number_portament:
-			channel.portament = float( value ) / 127.0
+			channel.portamento = float( value ) / 127.0
 		SMF.control_number_sostenuto:
 			channel.sostenuto = float( value ) / 127.0
 		SMF.control_number_freeze:
